22221589732661d36f2b0f8e93792fdc09a345f3
Mecheri <mohamed.mecheri@cgi.com>
2017-05-05 17:13:50 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SelectionBar from './SelectionBar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SelectionBar/>
+      </div>
+    );
+  }
+}
---
+++ src/locals.js
@@ -0,0 +1,25 @@
+module.exports ={
+    "days":[
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ],
+    "months":[
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+}
